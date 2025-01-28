49711941136ba5d20184e48a80b5a7652ce9748d
Victhor Caius <victhorcaius@gmail.com>
2025-01-28 09:29:05 +0700

    Copilot format and clean

--- composer.cpp
+++ composer.cpp
@@ -18,32 +18,31 @@ string getPath() {
 string checkPHP() {
     int check = system("where /q php.exe || exit 1");
     if (check == 0) {
-        system((char*) ("where php.exe > \"" + getPath() + "/php_path.txt\"").c_str());
-        ifstream in((char*) (getPath() + "/php_path.txt").c_str());
+        system(("where php.exe > \"" + getPath() + "/php_path.txt\"").c_str());
+        ifstream in((getPath() + "/php_path.txt").c_str());
         string php_path;
         getline(in, php_path);
         in.close();
 
         return php_path;
     }
-
     return "";
 }
 
 void installComposer() {
     cout << "Info: Dang tien hanh cai dat Composer" << endl;
     system("php -r \"copy('https://getcomposer.org/installer', 'composer-setup.php');\"");
-    system("php -r \"if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;\"");
-    system((char*) ("php composer-setup.php --install-dir \"" + getPath() + "\"").c_str());
+    system("php -r \"if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); }\"");
+    system(("php composer-setup.php --install-dir \"" + getPath() + "\"").c_str());
     system("php -r \"unlink('composer-setup.php');\"");
     cout << "Info: Cai dat Composer thanh cong" << endl;
     sleep(3);
     system("cls");
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     string php_path = checkPHP();
-    if (php_path != "") {
+    if (!php_path.empty()) {
         if (!filesystem::exists(getPath() + "/composer.phar")) {
             installComposer();
         }
@@ -57,7 +56,7 @@ int main(int argc, char *argv[]) {
             command.erase(command.find_last_not_of(" ") + 1);
         }
 
-        system((char*) command.c_str());
+        system(command.c_str());
     } else {
         cout << "Error: Vui long tai PHP" << endl;
     }
